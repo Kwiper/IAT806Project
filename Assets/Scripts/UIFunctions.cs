301d15dc6316d0ee96caf3fa14442fbbce2b5dba
Kwiper <jonny.tibboh@gmail.com>
2025-11-25 20:20:37 -0800

    Update UIFunctions.cs

--- Assets/Scripts/UIFunctions.cs
+++ Assets/Scripts/UIFunctions.cs
@@ -5,17 +5,17 @@ public class UIFunctions : MonoBehaviour
 {
     [SerializeField] InputAction tab, esc; // set variables for tab and esc buttons
 
-    [SerializeField] GameObject menu;
+    [SerializeField] GameObject menu; // assign quit menu gameobject
 
-    [SerializeField] GameObject ui;
+    [SerializeField] GameObject ui; // assign whole ui canvas 
 
     private void Awake()
     {
         tab.Enable();
         esc.Enable();
 
-        esc.performed += i => {if(ui.activeInHierarchy) ToggleQuitMenu(); };
-        tab.performed += i => {if(!menu.activeInHierarchy) ToggleUI(); };
+        esc.performed += i => {if(ui.activeInHierarchy) ToggleQuitMenu(); }; // Toggles the quit menu when esc is pressed
+        tab.performed += i => {if(!menu.activeInHierarchy) ToggleUI(); }; // Toggles the UI when tab is pressed
     }
 
     public void Quit()
